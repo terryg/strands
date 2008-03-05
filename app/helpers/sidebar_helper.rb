fe9c77f0d267d2f2133e42ec36027f5968132d9b
Terry Lorber <tgl@rideside.net>
2008-03-05 03:47:28 +0000

    Bad links.

--- app/helpers/sidebar_helper.rb
+++ app/helpers/sidebar_helper.rb
@@ -7,14 +7,14 @@ module SidebarHelper
       sidebars += "  <li><a href=\"/user/edit\">my profile</a></li>"
       sidebars += "  <li><a href=\"/strands\">strands</a></li>"
       sidebars += "  <li><a href=\"/nodes/tracker\">tracker</a></li>"
-      sidebars += "  <li><a href=\"timemap\">timemap</a></li>"
+      sidebars += "  <li><a href=\"/timemap\">timemap</a></li>"
       sidebars += "  <li><a href=\"/logout\">logout</a></li>"
       sidebars += "</ul>"
     else
       sidebars += "<ul>"
       sidebars += "  <li><a href=\"/strands\">strands</a></li>"
       sidebars += "  <li><a href=\"/nodes/tracker\">tracker</a></li>"
-      sidebars += "  <li><a href=\"timemap\">timemap</a></li>"
+      sidebars += "  <li><a href=\"/timemap\">timemap</a></li>"
       sidebars += "  <li><a href=\"/login\">login</a></li>"
       sidebars += "</ul>"    
     end
